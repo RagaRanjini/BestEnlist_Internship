88a1d31a4f22bb95bcdfc6eb4d0fec123ee1be72
RagaRanjini <74909515+RagaRanjini@users.noreply.github.com>
2020-11-29 07:17:08 -0800

    Update task 7 - (2).py

--- task 7 - (2).py
+++ task 7 - (2).py
@@ -1,3 +1,4 @@
+#installation of pandas package proof file is uploaded in github as "pandas package installation"
 import pandas 
 
 dict={"STUDENT NAME":"RAGA RANJINI","AGE":"19","GENDER":"FEMALE"}
