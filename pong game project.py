73247eaa00c6fd141cb14c542aeb854d16e0645f
RagaRanjini <74909515+RagaRanjini@users.noreply.github.com>
2020-12-07 01:38:24 -0800

    Add files via upload

---
+++ pong game project.py
@@ -0,0 +1,148 @@
+#once , you run the program , the game starts
+#one who use the left paddle , must use 'e' key and 'x' key to move the left paddle up and down respectively
+#one who use the right paddle, must use 'up' and 'down' key to move the right paddle up and down respectively
+#press 'x' key in the window at the right top corner to exit the game
+#the score of left and right paddle player gets displayed at the top of the game
+#when a player misses out to strike the ball, the opposite player's score gets incremented by +1
+#to win the game , make sure the ball strikes the middle of your paddle !
+
+import turtle 
+  
+  
+# Create screen 
+sc = turtle.Screen() 
+sc.title("Pong game") 
+sc.bgcolor("misty rose") 
+sc.setup(width=1000, height=600) 
+  
+  
+# Left paddle 
+left_pad = turtle.Turtle() 
+left_pad.speed(0) 
+left_pad.shape("square") 
+left_pad.color("IndianRed1") 
+left_pad.shapesize(stretch_wid=6, stretch_len=2) 
+left_pad.penup() 
+left_pad.goto(-400, 0) 
+  
+  
+# Right paddle 
+right_pad = turtle.Turtle() 
+right_pad.speed(0) 
+right_pad.shape("square") 
+right_pad.color("violet red") 
+right_pad.shapesize(stretch_wid=6, stretch_len=2) 
+right_pad.penup() 
+right_pad.goto(400, 0) 
+  
+  
+# Ball of circle shape 
+hit_ball = turtle.Turtle() 
+hit_ball.speed(60) 
+hit_ball.shape("circle") 
+hit_ball.color("blue4") 
+hit_ball.penup() 
+hit_ball.goto(0, 0) 
+hit_ball.dx = 5
+hit_ball.dy = -5
+  
+  
+# Initialize the score 
+playerA = 0
+playerB = 0
+  
+  
+# Displays the score 
+sketch = turtle.Turtle() 
+sketch.speed(0) 
+sketch.color("SlateBlue1") 
+sketch.penup() 
+sketch.hideturtle() 
+sketch.goto(0, 260) 
+sketch.write("playerA : 0    playerB : 0", 
+             align="center", font=("Gill sans MT", 24, "bold")) 
+  
+  
+# Functions to move paddle vertically 
+def paddleaup(): 
+    y = left_pad.ycor() 
+    y += 20
+    left_pad.sety(y) 
+  
+  
+def paddleadown(): 
+    y = left_pad.ycor() 
+    y -= 20
+    left_pad.sety(y) 
+  
+  
+def paddlebup(): 
+    y = right_pad.ycor() 
+    y += 20
+    right_pad.sety(y) 
+  
+  
+def paddlebdown(): 
+    y = right_pad.ycor() 
+    y -= 20
+    right_pad.sety(y)
+
+
+  
+  
+# Keyboard bindings 
+sc.listen() 
+sc.onkeypress(paddleaup, "e") 
+sc.onkeypress(paddleadown, "x") 
+sc.onkeypress(paddlebup, "Up") 
+sc.onkeypress(paddlebdown, "Down")
+
+  
+  
+while True: 
+    sc.update() 
+  
+    hit_ball.setx(hit_ball.xcor()+hit_ball.dx) 
+    hit_ball.sety(hit_ball.ycor()+hit_ball.dy) 
+  
+    # Checking borders 
+    if hit_ball.ycor() > 280: 
+        hit_ball.sety(280) 
+        hit_ball.dy *= -1
+  
+    if hit_ball.ycor() < -280: 
+        hit_ball.sety(-280) 
+        hit_ball.dy *= -1
+  
+    if hit_ball.xcor() > 500: 
+        hit_ball.goto(0, 0) 
+        hit_ball.dy *= -1
+        playerA += 1
+        sketch.clear() 
+        sketch.write("playerA : {}    playerB : {}".format( 
+                      playerA, playerB), align="center", 
+                      font=("Gill sans MT", 24, "bold")) 
+  
+    if hit_ball.xcor() < -500: 
+        hit_ball.goto(0, 0) 
+        hit_ball.dy *= -1
+        playerB += 1
+        sketch.clear() 
+        sketch.write("playerA : {}    playerB : {}".format( 
+                                 playerA, playerB), align="center", 
+                                 font=("Gill sans MT", 24, "bold")) 
+  
+    # Paddle ball collision 
+    if ((hit_ball.xcor() > 360 and
+                        hit_ball.xcor() < 370) and
+                        (hit_ball.ycor() < right_pad.ycor()+40 and
+                        hit_ball.ycor() > right_pad.ycor()-40)): 
+        hit_ball.setx(360) 
+        hit_ball.dx*=-1
+         
+    if ((hit_ball.xcor()<-360 and 
+                       hit_ball.xcor()>-370) and 
+                       (hit_ball.ycor()<left_pad.ycor()+40 and 
+                        hit_ball.ycor()>left_pad.ycor()-40)): 
+        hit_ball.setx(-360) 
+        hit_ball.dx*=-1
